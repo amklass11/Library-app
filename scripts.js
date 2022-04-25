006dc4459a63eb37c4e3d8b2f2c349e262c138ab
man lass <lassanaa14@gmail.com>
2022-04-25 13:59:50 -0600

    add book js adeed

--- scripts.js
+++ scripts.js
@@ -1,12 +1,10 @@
 let myLibrary=[];
 
 
-function AddBook(title,author) {
-    this.title=title;
-    this.author=author;
-
-    let book = new Book(title,author);
-    myLibrary.push(book);
+function Book(title,author) {
+    this.title= title;
+    this.author= author;
 }
- 
+let book= new Book(title,author);
+
 
