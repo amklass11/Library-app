3eeafa40165b2dfa9ee3c30d3e1ea719a3ce9ba6
man lass <lassanaa14@gmail.com>
2022-04-26 04:18:57 -0600

    fix linters

--- scripts.js
+++ scripts.js
@@ -1,50 +1,49 @@
-let myLibrary=[];
+let myLibrary = [];
 let newId = 1;
 
-function addBook(id, title,author) {
-    const book= {
-        id, title, author
-    }
-    myLibrary.push(book)
-    return book;
-}
-
-function newBook() {
-   const title= document.getElementById('title');
-   const author= document.getElementById('author');
-   const book= addBook(newId++, title.value, author.value);
-   addBookToShelf(book);
+function addBook(id, title, author) {
+  const book = {
+    id, title, author,
+  };
+  myLibrary.push(book);
+  return book;
 }
 
 function removeBook(id) {
-    myLibrary = myLibrary.filter(book => book.id != id);
-    return myLibrary;
+  myLibrary = myLibrary.filter((book) => book.id !== id);
+  return myLibrary;
 }
 
 function removeBookwin(event) {
-    const btn = event.target;
-    const value = btn.value;
-    removeBook(value);
-    btn.parentElement.remove();
+  const btn = event.target;
+  const { value } = btn;
+  removeBook(value);
+  btn.parentElement.remove();
 }
 
 function addBookToShelf(book) {
-    const bookShelf= document.getElementById('bookShelf');
-    bookShelf.setAttribute("style", "display:flex; flex-direction: column; margin-left: 43%;");
-    const bookdiv = document.createElement('div');
-    const bookcard = document.createElement('div');
-    bookcard.setAttribute("style", "text-align:center; width: 200px; display:flex; flex-direction: column; gap: 10px;");
-    bookcard.textContent = book.title+ " by " + book.author;
-    const removebtn = document.createElement('button');
-    removebtn.textContent = "remove";
-    removebtn.value = book.id;
-    removebtn.onclick = removeBookwin;
-    bookdiv.appendChild(bookcard);
-    bookcard.appendChild(removebtn);
-    bookShelf.appendChild(bookcard);
+  const bookShelf = document.getElementById('bookShelf');
+  bookShelf.setAttribute('style', 'display:flex; flex-direction: column; margin-left: 43%;');
+  const bookdiv = document.createElement('div');
+  const bookcard = document.createElement('div');
+  bookcard.setAttribute('style', 'text-align:center; width: 200px; display:flex; flex-direction: column; gap: 10px;');
+  bookcard.textContent = `${book.title} by ${book.author}`;
+  const removebtn = document.createElement('button');
+  removebtn.textContent = 'remove';
+  removebtn.value = book.id;
+  removebtn.onclick = removeBookwin;
+  bookdiv.appendChild(bookcard);
+  bookcard.appendChild(removebtn);
+  bookShelf.appendChild(bookcard);
 }
 
-localStorage.setItem(JSON.stringify({'MyLibrary': myLibrary}));
+function newBook() {
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
+  newId += newId + 1;
+  const book = addBook(newId, title.value, author.value);
+  addBookToShelf(book);
+}
 
-
-
+console.log(newBook('ddd', 'dd'));
+localStorage.setItem(JSON.stringify({ MyLibrary: myLibrary }));
