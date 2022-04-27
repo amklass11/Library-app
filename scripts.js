3a0fed230f4b44afff34099ad8ff85362096275b
man lass <lassanaa14@gmail.com>
2022-04-27 05:12:09 -0600

    Objects were replaced with a class

--- scripts.js
+++ scripts.js
@@ -1,49 +1,58 @@
-let myLibrary = [];
+const myLibrary = [];
 let newId = 1;
 
-function addBook(id, title, author) {
-  const book = {
-    id, title, author,
-  };
-  myLibrary.push(book);
-  return book;
+class Library {
+  addBook(id, title, author) {
+    this.id = id;
+    const book = {
+      id, title, author,
+    };
+    myLibrary.push(book);
+    return book;
+  }
+
+  removeBook(id) {
+    this.myLibrary = myLibrary.filter((book) => book.id !== id);
+    return myLibrary;
+  }
+
+  removeBookwin(event) {
+    const btn = event.target;
+    const { value } = btn;
+    this.removeBook(value);
+    btn.parentElement.remove();
+  }
+
+  addBookToShelf(book) {
+    const bookShelf = document.getElementById('bookShelf');
+    bookShelf.setAttribute('style', 'display:flex; flex-direction: column; margin-left: 43%;');
+    const bookdiv = document.createElement('div');
+    const bookcard = document.createElement('div');
+    bookcard.setAttribute('style', 'text-align:center; width: 200px; display:flex; flex-direction: column; gap: 10px;');
+    bookcard.textContent = `${book.title} by ${book.author}`;
+    const removebtn = document.createElement('button');
+    removebtn.textContent = 'remove';
+    removebtn.value = book.id;
+    removebtn.onclick = this.removeBookwin;
+    bookdiv.appendChild(bookcard);
+    bookcard.appendChild(removebtn);
+    bookShelf.appendChild(bookcard);
+  }
+
+  newBook() {
+    const title = document.getElementById('title');
+    const author = document.getElementById('author');
+    newId += newId + 1;
+    const book = this.addBook(newId, title.value, author.value);
+    this.addBookToShelf(book);
+  }
 }
 
-function removeBook(id) {
-  myLibrary = myLibrary.filter((book) => book.id !== id);
-  return myLibrary;
+function makeBook() {
+  new Library().newBook();
 }
 
-function removeBookwin(event) {
-  const btn = event.target;
-  const { value } = btn;
-  removeBook(value);
-  btn.parentElement.remove();
-}
+// localStorage.setItem(JSON.stringify({ MyLibrary: myLibrary }));
 
-function addBookToShelf(book) {
-  const bookShelf = document.getElementById('bookShelf');
-  bookShelf.setAttribute('style', 'display:flex; flex-direction: column; margin-left: 43%;');
-  const bookdiv = document.createElement('div');
-  const bookcard = document.createElement('div');
-  bookcard.setAttribute('style', 'text-align:center; width: 200px; display:flex; flex-direction: column; gap: 10px;');
-  bookcard.textContent = `${book.title} by ${book.author}`;
-  const removebtn = document.createElement('button');
-  removebtn.textContent = 'remove';
-  removebtn.value = book.id;
-  removebtn.onclick = removeBookwin;
-  bookdiv.appendChild(bookcard);
-  bookcard.appendChild(removebtn);
-  bookShelf.appendChild(bookcard);
-}
-
-function newBook() {
-  const title = document.getElementById('title');
-  const author = document.getElementById('author');
-  newId += newId + 1;
-  const book = addBook(newId, title.value, author.value);
-  addBookToShelf(book);
-}
-
-console.log(newBook('ddd', 'dd'));
-localStorage.setItem(JSON.stringify({ MyLibrary: myLibrary }));
+console.log(makeBook);
+console.log(myLibrary);
